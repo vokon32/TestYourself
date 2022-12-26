77d2f92101ee7125b921ebb60b06431d613583ec
Evgeniy <jenyleshenkoo@gmail.com>
2022-12-26 23:17:19 +0200

    addedRegistrationPage

--- testtesttest/Controllers/AccountController.cs
+++ testtesttest/Controllers/AccountController.cs
@@ -47,5 +47,40 @@ namespace testtesttest.Controllers
             TempData["Error"] = "Wron email credentials. Please, try again";
             return View(loginVM);
         }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "Email already in use";
+                return View(registerVM);
+            }
+            var newUser = new AppUser()
+            {
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress,
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            return RedirectToAction("Index", "Test");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Test");
+        }
     }
 }
