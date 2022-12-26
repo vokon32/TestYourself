afaf569ccebf68a342eb634b94d32b0f57a71c4f
Evgeniy <jenyleshenkoo@gmail.com>
2022-12-26 22:05:51 +0200

    addedLoginPage

---
+++ testtesttest/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using testtesttest.Data;
+using testtesttest.Models;
+using testtesttest.ViewModels;
+
+namespace testtesttest.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signinManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signinManager;
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Test");
+                    }
+                }
+                TempData["Error"] = "Wrong crednetials. Please, try again";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Wron email credentials. Please, try again";
+            return View(loginVM);
+        }
+    }
+}
